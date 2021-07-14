b392f359293458ce310f4d3cf5372de3c941b8af
WELLINGTON GOMES DOS SANTOS <wellington.gsantos9@senacsp.edu.br>
2021-07-13 22:56:33 -0300

    alterado

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from 'selenium-webdriver/http';
 import { Injectable } from '@angular/core';
+import * as internal from 'stream';
+
+/*export interface Cliente{
+  id: string;
+  nome: string;
+  cpf: string;
+  email: string;
+  telefone: string;
+  ativo: boolean;
+  senha: string;
+
+}*/
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 }
